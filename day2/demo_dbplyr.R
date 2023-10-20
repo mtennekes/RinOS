93a2a563779c126fcb462994858722526c38007b
Schalken <n.schalken@cbs.nl>
2023-10-20 11:48:48 +0200

    voorbeeld database

---
+++ day2/demo_dbplyr.R
@@ -0,0 +1,43 @@
+library(dplyr)
+library(dbplyr)
+
+# create a database
+con <- DBI::dbConnect(RSQLite::SQLite())
+
+# copy some data to the database
+DBI::dbWriteTable(con, "iris", iris)
+
+# retrieve a reference to the table in the database
+tbl_iris <- tbl(con, "iris")
+
+# this is now a link to the table in the database
+tbl_iris
+
+query <- 
+  tbl_iris %>% 
+  mutate(silly = 1)
+
+print(query)
+
+show_query(query)
+
+# this is now a link to the table in the database
+query <- 
+  tbl_iris %>% 
+  group_by(Species) %>% 
+  summarize( mean_sepal_length = mean(Sepal.Length, na.rm=TRUE)
+             , mean_sepal_width = mean(Sepal.Width, na.rm = TRUE)
+  ) %>% 
+  mutate(ratio = mean_sepal_length / mean_sepal_width)
+
+print(query)
+
+show_query(query)
+
+# you can also directly use SQL on the database
+DBI::dbGetQuery(con, "SELECT * FROM iris")
+
+### Close connection
+DBI::dbDisconnect(con)
+
+
